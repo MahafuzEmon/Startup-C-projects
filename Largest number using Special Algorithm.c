fb47a38e3f6e7ed77f2e4230ecc74b7de9d12f8e
Emon Hossain <debugger.emon.21@gmail.com>
2024-11-18 14:52:38 +0600

    Algorithm explanation added

--- Largest number using Special Algorithm.c
+++ Largest number using Special Algorithm.c
@@ -1,3 +1,4 @@
+//Find the largest number among 10 numbers using Linear Search or Linear Scan.
 #include<stdio.h>
 int main()
 {
@@ -8,10 +9,10 @@ int main()
         printf("%d= ", i+1); scanf("%d", &num[i]);
     }
 
-    comparer= num[0];
+    comparer= num[0]; //To find the largest number among 10 numbers, we let the 1st number be the maximum value and put it into a third party variable using which we will compare that to all other elements. Then we will compare that third party value to all other values to check if any of them is greater than the compared one. If so, we will overwrite that supposed value into the third party variable and after doing this for all other elements, we will end up getting the maximum value inside of the third party variable.
     for(int i=1; i<10; i++)
     {
-        if(num[i] > comparer)
+        if(num[i] > comparer) //So here, we copied the 1st element of the array to a third party variable and compared that to all the elements of the array linearly. This algorithm is called Linear Search or Linear Scan and can be helpful for more complicated stuff.
         {
             comparer=num[i];
         }
